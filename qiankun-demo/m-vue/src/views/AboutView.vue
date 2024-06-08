9897bfb60a9b23fd0fdb027e1c5f5fa0699b8ec2
yuzhongtao238910 <yzt238910@163.com>
2024-06-08 14:04:50 +0800

    3

--- qiankun-demo/m-vue/src/views/AboutView.vue
+++ qiankun-demo/m-vue/src/views/AboutView.vue
@@ -1,5 +1,13 @@
-<template>
+<template v-if="flag1">
   <div class="about">
     <h1>This is an about page</h1>
+    <template v-if="flag1">
+      <h2>222</h2>
+    </template>
   </div>
 </template>
+<script setup>
+  import { ref } from "vue"
+  const flag = ref(true)
+  const flag1 = ref(false)
+</script>
